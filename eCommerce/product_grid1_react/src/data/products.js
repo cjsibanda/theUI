678dc7ced3622ab0ee939156764bec8cda181c3b
cjsibanda <anesib100@gmail.com>
2026-05-26 00:59:40 -0400

    product_grid1_react setup

---
+++ eCommerce/product_grid1_react/src/data/products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    id: 1,
+    team: "Boks",
+    name: "Product 1",
+    price: 50.5
+  },
+  {
+    id: 2,
+    team: "Lions",
+    name: "Product 2",
+    price: 20.2
+  },
+  {
+    id: 3,
+    team: "Cheetahs",
+    name: "Product 3",
+    price: 10.1
+  },
+  {
+    id: 4,
+    team: "Sharks",
+    name: "Product 4",
+    price: 25.25
+  },
+  {
+    id: 5,
+    team: "Province",
+    name: "Product 5",
+    price: 12.34
+  },
+  {
+    id: 6,
+    team: "Stormers",
+    name: "Product 6",
+    price: 19.99
+  }
+];
+
+export default products;
