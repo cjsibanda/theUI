e1882c96ac093ea8bad6ead80b7e5b9952de1d1b
cjsibanda <anesib100@gmail.com>
2026-06-24 00:28:06 -0400

    The Hero

---
+++ Sites/sibanda-flowers/src/components/Hero.ts
@@ -0,0 +1,42 @@
+export function Hero() {
+  return `
+  
+  <section
+    class="
+      bg-white
+      flex
+      flex-col
+      items-center
+      justify-center
+      py-24
+    "
+  >
+
+    <h1
+      class="
+        text-5xl
+        font-bold
+        text-blue-600
+        mb-8
+      "
+    >
+      Get Summer Deals!!
+    </h1>
+
+    <button
+      class="
+        bg-blue-600
+        text-white
+        px-8
+        py-3
+        rounded-lg
+        hover:bg-blue-700
+      "
+    >
+      See Deals
+    </button>
+
+  </section>
+  
+  `;
+}
