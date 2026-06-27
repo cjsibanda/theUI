579b24ab16bad5f45807e87c89b7a235de7479cc
cjsibanda <anesib100@gmail.com>
2026-06-26 20:44:53 -0400

    added Services to App.ts

--- Sites/sibanda-flowers/src/App.ts
+++ Sites/sibanda-flowers/src/App.ts
@@ -3,6 +3,7 @@ import { Hero } from "./components/Hero";
 import { ProductGrid } from "./components/ProductGrid";
 import { CustomArrangement } from "./components/CustomArrangement";
 import { Footer } from "./components/Footer";
+import { Services } from "./components/Services";
 
 export function App() {
   return `git
@@ -11,6 +12,7 @@ export function App() {
     ${Hero()}
     ${ProductGrid()}
     ${CustomArrangement()}
+    ${Services()}
     ${Footer()}
   
   `;
