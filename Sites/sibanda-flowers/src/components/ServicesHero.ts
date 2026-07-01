e24699bfb17cb71602053095ede62a89bc9d73d2
cjsibanda <anesib100@gmail.com>
2026-07-01 15:55:54 -0400

    added Services Hero Component

---
+++ Sites/sibanda-flowers/src/components/ServicesHero.ts
@@ -0,0 +1,30 @@
+export function ServicesHero() {
+  return `
+  
+  <section
+    class="
+      relative
+      h-[500px]
+      bg-cover
+      bg-center
+      flex
+      items-center
+      justify-center
+    "
+    style="background-image: url('/src/assets/images/hero/hero.jpg');"
+  >
+
+    <div class="absolute inset-0 bg-black/40"></div>
+
+    <div class="relative text-center text-white">
+
+      <h1 class="text-5xl md:text-6xl font-bold">
+        Services
+      </h1>
+
+    </div>
+
+  </section>
+
+  `;
+}
