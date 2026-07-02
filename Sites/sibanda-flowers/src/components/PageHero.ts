f28857283eb60d3eae3b0edf040c7dac768a8dee
cjsibanda <anesib100@gmail.com>
2026-07-02 09:09:35 -0400

    updated PageHero to match Hero

--- Sites/sibanda-flowers/src/components/PageHero.ts
+++ Sites/sibanda-flowers/src/components/PageHero.ts
@@ -1,26 +1,49 @@
+import heroBackground from "../assets/images/hero-background.jpg";
+
 export function PageHero(title: string) {
   return `
   
   <section
     class="
       relative
-      h-[500px]
-      bg-cover
-      bg-center
       flex
       items-center
       justify-center
+      py-40
+      bg-cover
+      bg-center
     "
-    style="background-image: url('/src/assets/images/hero/hero.jpg');"
+    style="
+      background-image: url('${heroBackground}');
+    "
   >
 
-    <!-- Overlay -->
-    <div class="absolute inset-0 bg-black/40"></div>
+    <!-- Dark Overlay -->
+    <div
+      class="
+        absolute
+        inset-0
+        bg-black/40
+      "
+    ></div>
 
     <!-- Content -->
-    <div class="relative text-center text-white">
+    <div
+      class="
+        relative
+        z-10
+        text-center
+      "
+    >
 
-      <h1 class="text-5xl md:text-6xl font-bold">
+      <h1
+        class="
+          text-5xl
+          md:text-6xl
+          font-bold
+          text-white
+        "
+      >
         ${title}
       </h1>
 
