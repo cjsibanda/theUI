83c0dda43ec102369699af87fe9f395684b82562
cjsibanda <anesib100@gmail.com>
2026-07-04 19:39:24 -0400

    added config

---
+++ Sites/sibanda-flowers/src/config/company.ts
@@ -0,0 +1,18 @@
+export const company = {
+
+  name: "Sibanda Flowers",
+
+  tagline: "Wedding & Event Florist",
+
+  addressLine1: "1234 Main Street, Unit 1A",
+
+  addressLine2: "Toronto, ON A1B 2C3",
+
+  phone: "(647) 123-4567",
+
+  copyright: "© 2026 Sibanda Flowers. All rights reserved.",
+
+  slogan:
+    "Beautiful flowers for weddings, events & life's special moments."
+
+};
