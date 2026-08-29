afbc55b80d2588292e21eb53cecde6f2ac0672dd
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-08-29 00:46:12 +0000

    changed base in vite.congig.ts

--- Sites/sibanda-flowers/vite.config.ts
+++ Sites/sibanda-flowers/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/theUI/sibanda-flowers/',
+  base: '/theUI/',
   plugins: [tailwindcss()]
 })
